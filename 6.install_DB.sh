6b3626c990fb0c2664645873a5166d8bb7a2a046
lenghongmeng <lenghongmeng98@gmail.com>
2023-09-17 17:39:54 +0700

    imp

--- 6.install_DB.sh
+++ 6.install_DB.sh
@@ -1,14 +1,14 @@
 #delete container if it exist for org 1
-docker container stop couchdb
-docker container rm -f couchdb
+sudo docker container stop couchdb
+sudo docker container rm -f couchdb
 
 #delete container if it exist for org 2
-docker container stop couchdb2
-docker container rm -f couchdb2
+sudo docker container stop couchdb2
+sudo docker container rm -f couchdb2
 
 #start two database container
-docker run -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password --name=couchdb 5984:5984 -d couchdb:3.1.1 --restart unless-stopped
-docker run -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password --name=couchdb2 5985:5984 -d couchdb:3.1.1 --restart unless-stopped
+sudo docker run -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password --name=couchdb -p 5984:5984 -d couchdb:3.1.1 --restart unless-stopped
+sudo docker run -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password --name=couchdb2 -p 5985:5984 -d couchdb:3.1.1 --restart unless-stopped
 
 sleep 3s
 
--- start_orderer.sh
+++
@@ -1,4 +0,0 @@
-export FABRIC_CFG_PATH=${PWD}/config
-export CORE_PEER_MSPCONFIGPATH=${PWD}/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-
-orderer start
